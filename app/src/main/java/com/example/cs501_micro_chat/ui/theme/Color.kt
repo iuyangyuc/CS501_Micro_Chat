6af7ce907ed96c0dfafd6a0c551a47f4da87c779
XXXQIVX <82208900+XXXQIVX@users.noreply.github.com>
2025-10-11 17:23:33 -0400

    Adding Dark and light color theme

--- app/src/main/java/com/example/cs501_micro_chat/ui/theme/Color.kt
+++ app/src/main/java/com/example/cs501_micro_chat/ui/theme/Color.kt
@@ -3,10 +3,48 @@ package com.example.cs501_micro_chat.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Brand blues
-val NavyBlue = Color(0xFF00296B)
-val DeepBlue = Color(0xFF003F88)
 val PrimaryBlue = Color(0xFF00509D)
+val PrimaryBlueBright = Color(0xFFA8C7FF)
+val PrimaryBlueContainerLight = Color(0xFFD6E4FF)
+val PrimaryBlueContainerDark = Color(0xFF003F79)
+val NavyBlue = Color(0xFF001B3C)
+val NavyNight = Color(0xFF0B1626)
+val DeepBlue = Color(0xFF003F88)
+val SecondaryContainerLight = Color(0xFFDCE3FF)
+val SecondaryContainerDark = Color(0xFF294973)
+val SecondaryBlueBright = Color(0xFFAEC6FF)
 
 // Accent golds
 val WarmGold = Color(0xFFFDC500)
-val BrightGold = Color(0xFFFFD500)
+val BrightGold = Color(0xFFFFE089)
+val DarkGold = Color(0xFF3A2D00)
+
+// Neutral surfaces
+val SurfaceBackgroundLight = Color(0xFFFFFBFE)
+val SurfaceBackgroundDark = Color(0xFF0F141C)
+val SurfaceOnLight = Color(0xFF1B2536)
+val SurfaceOnDark = Color(0xFFE1E6F3)
+val SurfaceVariantLight = Color(0xFFE0E6F4)
+val SurfaceVariantDark = Color(0xFF3E4557)
+val SurfaceBrightLight = Color(0xFFF7F9FF)
+val SurfaceBrightDark = Color(0xFF202838)
+val SurfaceDimLight = Color(0xFFE9EDF7)
+val SurfaceDimDark = Color(0xFF101522)
+
+// Surface containers
+val SurfaceContainerLowestLight = Color(0xFFFFFFFF)
+val SurfaceContainerLowLight = Color(0xFFF3F6FC)
+val SurfaceContainerLight = Color(0xFFE0E6F4)
+val SurfaceContainerHighLight = Color(0xFFD2DAEB)
+val SurfaceContainerHighestLight = Color(0xFFC3CDE2)
+val SurfaceContainerLowestDark = Color(0xFF080E18)
+val SurfaceContainerLowDark = Color(0xFF101522)
+val SurfaceContainerDark = Color(0xFF191F2D)
+val SurfaceContainerHighDark = Color(0xFF222A39)
+val SurfaceContainerHighestDark = Color(0xFF2C3444)
+
+// Outlines
+val OutlineLight = Color(0xFF5E6A7C)
+val OutlineDark = Color(0xFF8C92A3)
+val OutlineVariantLight = Color(0xFFC4CAD6)
+val OutlineVariantDark = Color(0xFF444B5C)
